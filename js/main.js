39bfa36d3f5c535f7763b01a0313912289aa963c
Andre Milton <wredhyn@gmail.com>
2016-12-27 20:52:49 +0000

    added highlight scale

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 AFRAME.registerComponent('highlight', {
     init: function () {
         var color = this.el.attributes.color.value;
-        var highlight = this.data.color;
+        var scale = this.el.attributes.scale.value;
+        var highlightColor = this.data.color;
+        var highlightScale = this.data.scale;
         
         this.el.addEventListener('mouseenter', function () {
-            this.setAttribute('color', highlight);
+            this.setAttribute('color', highlightColor);
+            this.setAttribute('scale', highlightScale);
         });
         this.el.addEventListener('mouseleave', function () {
             this.setAttribute('color', color);
+            this.setAttribute('scale', scale);
         });
     }
 });
