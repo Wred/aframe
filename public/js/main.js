cb207332efe258a6cbd4600e64f651626ec22607
andre <andre@mediafresh.net>
2016-12-30 22:26:58 -0500

    added socket.io and moved client to public folder

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,12 @@
+var socket = io();
+
 AFRAME.registerComponent('highlight', {
     init: function () {
         var color = this.el.attributes.color.value;
         var scale = this.el.attributes.scale.value;
         var highlightColor = this.data.color;
         var highlightScale = this.data.scale;
-        
+
         this.el.addEventListener('mouseenter', function () {
             this.setAttribute('color', highlightColor);
             this.setAttribute('scale', highlightScale);
